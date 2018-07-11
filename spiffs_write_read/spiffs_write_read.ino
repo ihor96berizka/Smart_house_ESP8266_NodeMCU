959561fbf5edcda3f5a455bc34145acc5a54f468
Ihor Berizka <ihor.berizka@gmail.com>
2018-07-11 20:01:41 +0300

    First versions of test modules

---
+++ spiffs_write_read/spiffs_write_read.ino
@@ -0,0 +1,40 @@
+#include "FS.h"
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // always use this to "mount" the filesystem
+  bool result = SPIFFS.begin();
+  Serial.println("SPIFFS opened: " + result);
+
+  // this opens the file "f.txt" in read-mode
+  File f = SPIFFS.open("/cl_conf.txt", "r");
+  
+  if (!f) {
+    Serial.println("File doesn't exist yet. Creating it");
+
+    // open the file in write mode
+    File f = SPIFFS.open("/cl_conf.txt", "w");
+    if (!f) {
+      Serial.println("file creation failed");
+    }
+    // now write two lines in key/value style with  end-of-line characters
+    f.println("msi_wifi");
+    f.println("11111111");
+  } else {
+    // we could open the file
+    while(f.available()) {
+      //Lets read line by line from the file
+      String line = f.readStringUntil('n');
+      Serial.println(line);
+    }
+
+  }
+  f.close();
+}
+
+void loop() {
+  // nothing to do for now, this is just a simple test
+
+}
---
+++ test_OTA_and_SFIFFS/test_OTA_and_SFIFFS.ino
@@ -0,0 +1,98 @@
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoOTA.h>
+#include <FS.h>
+
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+const byte led = D5;
+
+void readSPIFFS();
+
+void setup() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  wifiMulti.addAP("msi_wifi", "11111111");   // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+  
+  ArduinoOTA.setHostname("ESP8266");
+  ArduinoOTA.setPassword("esp8266");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 1);
+}
+
+unsigned long previousTime = millis();
+
+const unsigned long interval = 500;
+
+void loop() {
+  ArduinoOTA.handle();
+  unsigned long diff = millis() - previousTime;
+  if(diff > interval) {
+    digitalWrite(led, !digitalRead(led));  // Change the state of the LED
+    previousTime += diff;
+    readSPIFFS();
+   // delay(100);
+  }
+}
+
+void readSPIFFS()
+{
+    // always use this to "mount" the filesystem
+  bool result = SPIFFS.begin();
+  Serial.println("SPIFFS opened: " + result);
+
+  // this opens the file "f.txt" in read-mode
+  File f = SPIFFS.open("/cl_conf.txt", "r");
+  
+  if (!f) {
+    Serial.println("File doesn't exist yet.");
+  } else {
+    // we could open the file
+    while(f.available()) {
+      //Lets read line by line from the file
+      String line = f.readStringUntil('n');
+      Serial.println(line);
+    }
+  }
+  f.close();
+}
+
